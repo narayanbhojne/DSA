d6fe44c00c6d3e4bc85dcb687dac8644666ce998
narayanbhojne <narayanbhojne2000@gmail.com>
2024-06-23 16:06:35 +0530

    array Creat

---
+++ Array/Creation.java
@@ -0,0 +1,14 @@
+package DSA.Array;
+
+public class Creation {
+    
+    public static void main(String[] args) {
+    
+    int arr[]={1,2,3};
+    for(int i=0; i<arr.length; i++)
+    {
+        System.out.println(arr[i]);
+    }
+    }
+    
+}
